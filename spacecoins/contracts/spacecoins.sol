01e864925d4511254c3ef75b1a6a97980e01d754
deepsi <deepsim543@gmail.com>
2021-02-23 20:39:24 +0530

    spacecoinb

---
+++ spacecoins/contracts/spacecoins.sol
@@ -0,0 +1,19 @@
+pragma ^0.5.25;
+
+import "openzepplin-solidity/contracts/token/ERC20/ERC20.sol;
+
+contract spacecoins is  ERC-20{
+    uint public number;
+    uin256 _totalsupply;
+    address sender;
+    mapping(address=>uint) balances;
+     events(address from, address to,uint balance)
+
+    function _mint(address send,uint balance){
+      require(balance > 0){
+         uint sender = msg.sender;
+         balance = msg.value;
+      }
+    }
+    
+}
